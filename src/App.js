2d8707a10c71cd7a36b75463084ce8fab24b15fb
shubham <shubham805822@gmail.com>
2023-06-29 00:23:14 +0530

    trial 1.1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<SearchPage />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
